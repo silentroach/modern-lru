4d34029ee4f532251fab4bce78a41a6fdc0785b0
Igor Kalashnikov <igor.kalashnikov@me.com>
2019-02-12 09:54:07 +0300

    index.d.ts, refs #2

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export default class ModernLRU<K, V> extends Map<K, V> {
+    /**
+     * Cache keys limit in current instance
+     */
+    public readonly limit: number;
+
+    constructor(limit: number, initial?: ReadonlyArray<[K, V]> | null);
+}
